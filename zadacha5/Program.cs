e0317fb3b6e9a25e560c5ca9f7c551a0165100a1
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-16 23:39:50 +0300

    решили задачу является ли 1 число квадратом 2

---
+++ zadacha5/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Принимает на вход два числа и проверяет, является ли одно число квадратом второго
+*/ 
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b % a == 0)
+Console.WriteLine($"{a} является квадратом {b}");
+else
+Console.WriteLine($"{a} не является квадратом {b}");
+
