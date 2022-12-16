c3954245d128b65855eca7b7289209f2a300af0e
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-16 23:09:33 +0300

    решили задачу на определение кратно ли 1 число другому,  если нет, вывели отсаток

---
+++ zadacha3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*напишите программу, которая на вход принимает 2 числа и выводит, является ли первое число кратным второму.
+если число 1 не кратно второму, выводится остаток от деления 
+*/
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a % b == 0)
+    Console.WriteLine($"{a},{b} -> кратно");
+else
+{
+    Console.WriteLine($"{a},{b} -> не кратно");
+    Console.WriteLine(a % b);
+}
