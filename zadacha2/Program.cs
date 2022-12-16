35b1e28eb584de044f5e78f43948507fc8101d58
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-16 21:37:27 +0300

    сделали задачу на генирацию 3-х значного числа и удаления второго символа

---
+++ zadacha2/Program.cs
@@ -0,0 +1,7 @@
+﻿ Random random = new Random();
+int number = random.Next(100, 1000);
+Console.WriteLine($"число {number}");
+
+int a = number / 100;
+int c = number % 10;
+Console.WriteLine($"{a}{c}");
