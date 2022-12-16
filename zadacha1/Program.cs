a0da51ddcc20afedabc98c3176b81b1706a24b99
Darina Tsaprilava <doratsaprilava@gmail.com>
2022-12-16 20:31:08 +0300

    первая задача на рандомное число от 10 до 99 и определения какое число из цифры больше

---
+++ zadacha1/Program.cs
@@ -0,0 +1,14 @@
+﻿Random random = new Random();
+int number = random.Next(10, 100);
+Console.WriteLine("Random number = {0}", number);
+
+int a = number / 10;
+int b = number % 10;
+
+if (a > b)
+    Console.WriteLine("a > b, {0}", a);
+else if (a < b)
+    Console.WriteLine("a < b, {0}", b);
+else
+
+Console.WriteLine("a = b");
